1010eb88fdf058e8f10774c8fc4a711359f99d92
Julian -Home <j.henry.b.orr@gmail.com>
2018-10-30 14:34:50 -0400

    Added app.cs

---
+++ App.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace Kata
+{
+    class App
+    {
+        static void Main(string[] vs)
+        {
+            Console.WriteLine("Please enter a number");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            RomanConverter converter = new RomanConverter();
+            string finalResult = converter.Convert(userNumber);
+            Console.WriteLine($"Your result is {finalResult}.");
+            Console.ReadLine();
+        }
+    }
+}
